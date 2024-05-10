6efa94d8f1f6330ce38bc6c30cdbc880f5d4fc5c
cmquintero <cmquinterot@ufpso.edu.co>
2024-05-10 18:00:08 -0500

    socio

---
+++ src/components/vue/ToDo.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+const todos = ref(['Estudiar bastante' ])
+const todo = ref('')
+function add() {
+todos.value.unshift(todo.value)
+todo.value=''
+}
+</script>
+<template>
+    <div class=" py-6 px-5 rounded shadow bg-slate-300 max-w-96 mx-auto mt-10">
+        <div>
+            <input v-model="todo" class=" px-3  w-4/5 outline-none rounded " />
+            <button @click="add" class=" w-1/5 bg-green-400 text-white text-center">+</button>
+        </div>
+        <div 
+        v-for=" (todo, i) in todos"
+        :key="i"
+        class=" px-1  mt-3 bg-orange-300 rounded text-center ">{{ todo}}</div>
+    </div>
+</template>
