aaf9f7307a6966e663f2c3fe69508ee2e44d73cd
cmquintero <cmquinterot@ufpso.edu.co>
2024-05-10 17:59:33 -0500

    first commit

---
+++ src/components/vue/Directives.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+const users= ["pepe", "Juana ", "Andrea", "lola"] 
+const isChecked = ref(true);
+</script>
+<template>
+    <ul>
+        <li v-for="( user, index) in users" :key=" index ">{{ index }} {{ user }} </li>
+    </ul>
+    <input type="checkbox" id="check" v-model="isChecked"/>
+    <label for="check"> Pick</label>
+    <div class=" bg-blue-400  text-white rounded py-1 px-2 w-40 mx-auto" :class="{' bg-orange-400' : isChecked}">Este es un mensaje  </div>
+    <div v-show="isChecked"> Este es un mensaje que se podria ocultar</div>
+    <div v-if="isChecked">Este es otro mensaje que se podria ocultar </div>
+    <div v-else> Este mensajesi se podria mostrar ya que cumple la condicion</div>
+</template>
